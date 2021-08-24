a7c6015606b9f6f81e0f999a090f8cf03ecf31e7
Marc Pampols <mpampols@mac-mpampols.local>
2021-08-24 23:21:39 +0200

    Add 6.19

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,9 +1,11 @@
 
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notifications)
+const Notification = (props) => {
+  const notification = () => {
+    return props.notifications
+  }
 
   const style = {
     border: 'solid',
@@ -11,15 +13,23 @@ const Notification = () => {
     borderWidth: 1
   }
 
-  if (notification === null) {
+  if (notification() === null) {
     return null
   }
 
   return (
     <div style={style}>
-      {notification}
+      {notification()}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notifications: state.notifications,
+  }
+}
+
+const ConnectedNotifications = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotifications
